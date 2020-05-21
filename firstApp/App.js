b47a1c51639fef4cfbb3a9cd75a9e24506330ad8
theoriginalsam <spoudel04@gmail.com>
2020-05-21 20:01:10 +0545

    commit via script

--- firstApp/App.js
+++ firstApp/App.js
@@ -1,21 +1,20 @@
-
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-import Home from "./Components/Home";
-import List from "./Components/List";
-import React from 'react'
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import Home from './Components/Home';
+import List from './Components/List';
+import React from 'react';
 
 const navigator = createStackNavigator(
   {
-    Home:Home,
-    List: List
+    Home: Home,
+    List: List,
   },
   {
-    initialRouteName: "List",
+    initialRouteName: 'List',
     defaultNavigationOptions: {
-      title: "App"
-    }
-  }
+      title: 'App',
+    },
+  },
 );
 
 export default createAppContainer(navigator);
--- firstApp/Components/List.js
+++ firstApp/Components/List.js
@@ -1,43 +1,30 @@
-
 import React from 'react';
-import {
-  
-  StyleSheet,
-  View,
-  Text,
-  FlatList
-  
- } from 'react-native';
+import {StyleSheet, View, Text, FlatList} from 'react-native';
 
-const List = ()=>{
-     const friends=[
-         {name:"Samir"},
-         {name:"Sujan"},
-         {name:"Arjun"},
-         {name:"Sumit"}
-     ]
+const List = () => {
+  const friends = [
+    {name: 'Samir'},
+    {name: 'Sujan'},
+    {name: 'Arjun'},
+    {name: 'Sumit'},
+  ];
 
-  return(
-
-
-    <FlatList 
-    data={friends}
-    renderItem={ ({item})=>{
-   return <Text>{item.name}</Text>
-    }
-
-    } />
-    
-
-       
-
-  )
-}
-
+  return (
+    <FlatList
+      horizontal
+      keyExtractor={friend => friend.name}
+      data={friends}
+      renderItem={({item}) => {
+        return <Text style={styles.textStyle}>{item.name}</Text>;
+      }}
+    />
+  );
+};
 
 const styles = StyleSheet.create({
-
-})
-
+  textStyle: {
+    marginVertical: 200,
+  },
+});
 
 export default List;
