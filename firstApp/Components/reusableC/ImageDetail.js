194493e5123fbcb9d785bbaaeea392f8c54a0eff
theoriginalsam <spoudel04@gmail.com>
2020-05-23 14:46:17 +0545

    commit via script

---
+++ firstApp/Components/reusableC/ImageDetail.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+const ImageScree = () => {
+  return <Text>And List</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default ImageScree;
