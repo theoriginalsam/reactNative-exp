f4e9a8a794dd064ae06a34f8d199f3c6eb3bebc8
theoriginalsam <spoudel04@gmail.com>
2020-05-25 13:38:51 +0545

    Commit via script

--- firstApp/Components/state/TextScreen.js
+++ firstApp/Components/state/TextScreen.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, TextInput} from 'react-native';
 
 const TextScreen = () => {
   return (
     <View>
       <Text>Hello</Text>
+      <TextInput style={styles.input} />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    margin: 10,
+    borderColor: 'black',
+    borderWidth: 2,
+  },
+});
 
 export default TextScreen;
