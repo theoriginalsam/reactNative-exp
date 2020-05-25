38ff3a59bc872720be3977f9e1daca0601de03bc
theoriginalsam <spoudel04@gmail.com>
2020-05-25 13:53:07 +0545

    Commit via script

--- firstApp/Components/state/TextScreen.js
+++ firstApp/Components/state/TextScreen.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, View, Text, TextInput} from 'react-native';
 
 const TextScreen = () => {
+  const [string, setString] = useState('');
   return (
     <View>
       <Text>Hello</Text>
-      <TextInput style={styles.input} />
+      <TextInput
+        style={styles.input}
+        value={string}
+        onChangeText={newValue => setString(newValue)}
+      />
+      <Text>My Name is {string}</Text>
     </View>
   );
 };
