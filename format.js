f0661b18daa0a3c5cb2d3e27167a15d339d860a3
theoriginalsam <spoudel04@gmail.com>
2020-05-25 13:55:49 +0545

    Commit

---
+++ format.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+const List = () => {
+  return <View> </View>;
+};
+
+const styles = StyleSheet.create({});
+
+export default List;
