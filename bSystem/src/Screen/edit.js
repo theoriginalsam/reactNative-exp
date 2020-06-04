9094b90a6f27e3f920130d1def06e5fee66e6e90
theoriginalsam <spoudel04@gmail.com>
2020-06-04 18:33:29 +0545

    commit via script

--- bSystem/src/Screen/edit.js
+++ bSystem/src/Screen/edit.js
@@ -32,7 +32,7 @@ const Edit = ({navigation}) => {
         placeholder="Enter the Content "
       />
       <Button
-        title="EDIT "
+        title="EDIT"
         onPress={() => {
           editBlog(id, title, body);
           navigation.navigate('Index');
