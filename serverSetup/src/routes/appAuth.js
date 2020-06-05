ab61a38bfb57f08009c9947097f6ee25d2e128f2
theoriginalsam <spoudel04@gmail.com>
2020-06-05 22:18:34 +0545

    commit via script

--- serverSetup/src/routes/appAuth.js
+++ serverSetup/src/routes/appAuth.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/signup", (req, res) => {
-  res.send("You mad a request");
+  res.send("You made a request");
   console.log(req.body);
 });
 
