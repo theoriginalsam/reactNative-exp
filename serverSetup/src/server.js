7825cf1a29f426f3c939f6554b5effa087e5107c
theoriginalsam <spoudel04@gmail.com>
2020-06-05 19:56:09 +0545

    commit via script

--- serverSetup/src/server.js
+++ serverSetup/src/server.js
@@ -1,7 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
+const mongoUri =
+  "mongodb+srv://samir:samir@cluster0-sj8rp.mongodb.net/<dbname>?retryWrites=true&w=majority";
+
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected");
+});
+mongoose.connection.on("error", () => {
+  console.log("Error");
+});
 app.get("./", (req, res) => {
   res.send("Express called");
 });
