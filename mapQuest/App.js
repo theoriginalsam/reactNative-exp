8968fdf82a2c74d31d1ebb20d06e7210c40fb423
theoriginalsam <spoudel04@gmail.com>
2020-06-06 21:25:49 +0545

    commit via script

--- mapQuest/App.js
+++ mapQuest/App.js
@@ -43,7 +43,7 @@ const App: () => React$Node = () => {
               <Text style={styles.sectionTitle}>Step One</Text>
               <Text style={styles.sectionDescription}>
                 Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then cme back to see your edits.
+                screen and then cme back to see our edits.
               </Text>
             </View>
             <View style={styles.sectionContainer}>
