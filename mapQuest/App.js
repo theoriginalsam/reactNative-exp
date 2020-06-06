eef8319ced43fa18171362f1baebddc801a2eeca
theoriginalsam <spoudel04@gmail.com>
2020-06-06 21:26:23 +0545

    commit via script

--- mapQuest/App.js
+++ mapQuest/App.js
@@ -47,7 +47,7 @@ const App: () => React$Node = () => {
               </Text>
             </View>
             <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See You Ches</Text>
+              <Text style={styles.sectionTitle}>See Ches</Text>
               <Text style={styles.sectionDescription}>
                 <ReloadInstructions />
               </Text>
