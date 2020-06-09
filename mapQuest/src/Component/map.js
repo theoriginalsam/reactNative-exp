405be4e1867f6e7406623953880d5353cc950164
theoriginalsam <spoudel04@gmail.com>
2020-06-09 20:49:16 +0545

    commit via script

--- mapQuest/src/Component/map.js
+++ mapQuest/src/Component/map.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import {StyleSheet, View, Text} from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, {Polyline} from 'react-native-maps';
 
 const Map = () => {
-  return <MapView style={styles.map} />;
+  let points = [];
+  for (let i = 0; i < 20; i++)
+    points.push({
+      latitude: 28.2096 + i * 0.001,
+      longitude: 83.9856 + i * 0.001,
+    });
+  return (
+    <MapView
+      initialRegion={{
+        latitude: 28.2096,
+        longitude: 83.9856,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+      }}
+      style={styles.map}>
+      <Polyline coordinates={points} />
+    </MapView>
+  );
 };
 
 const styles = StyleSheet.create({
--- mapQuest/src/Screen/TrackCreate.js
+++ mapQuest/src/Screen/TrackCreate.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {StyleSheet, View} from 'react-native';
 import {Text, Input} from 'react-native-elements';
 import {SafeAreaView} from 'react-navigation';
+import {requestPermissionsAsync} from 'expo-location';
+
 import Map from '../Component/map';
 
 const TrackCreate = () => {
+  const [err, setErr] = useState(null);
+
+  const startTrack = async () => {
+    try {
+      await requestPermissionsAsync();
+    } catch (e) {
+      setErr(e);
+    }
+  };
   return (
     <View>
       <Text h1>Track Create</Text>
