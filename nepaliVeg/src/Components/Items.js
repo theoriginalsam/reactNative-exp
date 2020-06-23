2d1fd175d673f3595082e57b687224621b501982
theoriginalsam <spoudel04@gmail.com>
2020-06-23 21:41:14 +0545

    acommit via script

--- nepaliVeg/src/Components/Items.js
+++ nepaliVeg/src/Components/Items.js
@@ -1,8 +1,9 @@
 import React, {useState} from 'react';
 import {StyleSheet, View, Text, FlatList, Image, Button} from 'react-native';
 
-import {TouchableOpacity} from 'react-native-gesture-handler';
+import {TouchableOpacity, ScrollView} from 'react-native-gesture-handler';
 import SearchC from '../Components/SearchC';
+import Footer from './footer';
 const Items = ({navigation, results}) => {
   const [string, setString] = useState('');
   var resultName = [];
@@ -10,6 +11,7 @@ const Items = ({navigation, results}) => {
   for (var i in results) {
     resultName.push([i]);
   }
+  resultName.pop();
   var matched = [];
   console.log(string[0]);
   // console.log(resultName[1][0][0]);
@@ -23,69 +25,71 @@ const Items = ({navigation, results}) => {
   console.log(unique);
 
   return (
-    <View style={styles.container}>
-      <View style={styles.imageB}>
-        <Image source={require('../../assets/3.jpg')} style={styles.logo} />
+    <ScrollView>
+      <View style={styles.container}>
+        <View style={styles.imageB}>
+          <Image source={require('../../assets/3.jpg')} style={styles.logo} />
+        </View>
+        <SearchC
+          // style={styles.fontS}
+          string={string}
+          onTermChange={newString => setString(newString)}
+        />
+        {string.length == 0 ? (
+          <View style={styles.container}>
+            <FlatList
+              data={resultName}
+              keyExtractor={item => item}
+              renderItem={({item}) => {
+                return (
+                  <TouchableOpacity
+                    onPress={() => {
+                      navigation.navigate('IndScreen', {
+                        results: results,
+                        id: item,
+                      });
+                    }}>
+                    <View>
+                      <Text style={styles.items}>{item}</Text>
+                    </View>
+                  </TouchableOpacity>
+                );
+              }}
+            />
+          </View>
+        ) : (
+          <View style={styles.container}>
+            <FlatList
+              data={unique}
+              keyExtractor={item => item}
+              renderItem={({item}) => {
+                return (
+                  <TouchableOpacity
+                    onPress={() => {
+                      navigation.navigate('IndScreen', {
+                        results: results,
+                        id: item,
+                      });
+                    }}>
+                    <View>
+                      <Text style={styles.items}>{item}</Text>
+                    </View>
+                  </TouchableOpacity>
+                );
+              }}
+            />
+          </View>
+        )}
+        <Footer />
       </View>
-      <SearchC
-        // style={styles.fontS}
-        string={string}
-        onTermChange={(newString) => setString(newString)}
-      />
-      {string.length == 0 ? (
-        <View style={styles.container}>
-          <FlatList
-            data={resultName}
-            keyExtractor={(item) => item}
-            renderItem={({item}) => {
-              return (
-                <TouchableOpacity
-                  onPress={() => {
-                    navigation.navigate('IndScreen', {
-                      results: results,
-                      id: item,
-                    });
-                  }}>
-                  <View>
-                    <Text style={styles.items}>{item}</Text>
-                  </View>
-                </TouchableOpacity>
-              );
-            }}
-          />
-        </View>
-      ) : (
-        <View style={styles.container}>
-          <FlatList
-            data={unique}
-            keyExtractor={(item) => item}
-            renderItem={({item}) => {
-              return (
-                <TouchableOpacity
-                  onPress={() => {
-                    navigation.navigate('IndScreen', {
-                      results: results,
-                      id: item,
-                    });
-                  }}>
-                  <View>
-                    <Text style={styles.items}>{item}</Text>
-                  </View>
-                </TouchableOpacity>
-              );
-            }}
-          />
-        </View>
-      )}
-    </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flexWrap: 'nowrap',
     backgroundColor: 'white',
-    marginBottom: 100,
+    marginBottom: 30,
   },
 
   items: {
